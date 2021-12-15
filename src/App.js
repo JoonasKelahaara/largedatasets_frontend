1913c0bbe7398d21863095e91e792a32e75f9873
Joonas Kelahaara <jolle.kela@gmail.com>
2021-12-16 01:38:29 +0200

    lisää nappeja ja toimivuuksia

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ export default function App() {
   //Muuttujat, joita tarvitaan tiedon näyttämiseen ja hakemiseen
   const url = 'http://localhost/largedatasets/'
   const [searchTerm, setSearchTerm] = useState('');
+  const [radioSearch, setRadioSearch] = useState('');
   const [results, setResults] = useState([]);
+  const [viewResults, setViewResults] = useState([]);
 
   const search = e => {                           //Funktio, jolla lähetetään hakutermi queryparametrinä backendiin ja sillä haetaan tulos
     e.preventDefault();
 
-    if(searchTerm === ''){
+    if(searchTerm === ''){                        //Jos hakuehtoa ei ole asetettu, niin ei haeta mitään
       return;
     } else {
 
@@ -22,23 +24,101 @@ export default function App() {
       const json = response.data;
       console.log(json);
       setResults(json);                                         //Asetetaan tulos taulukkoon
-    }). catch(e => console.log(e))
+      setViewResults([]);
+    }).catch(e => console.log(e))
   }}
+
+  const radio = e => {
+    e.preventDefault();
+
+    axios.get(url + '/radio.php?search=' + radioSearch
+    ).then((response) => {
+      const json = response.data;
+      setResults(json);
+    }).catch(e => console.log(e))
+  }
+
+  const fi_movies = e => {
+    e.preventDefault();
+
+    axios.get(url + '/finnish.php'
+    ).then((response) => {
+      const json = response.data;
+      setViewResults(json);
+      setResults([]);
+    }).catch(e => console.log(e))
+  }
+
+  const gb_movies = e => {
+    e.preventDefault();
+
+    axios.get(url + '/english.php'
+    ).then((response) => {
+      const json = response.data;
+      setViewResults(json);
+      setResults([]);
+    }).catch(e => console.log(e))
+  }
 
   return (
     <div className="container">
       <h2>Find movies based on search terms</h2>
       <label className="myForm label-text">Insert search term: </label>
       <input className="myForm" onChange={(e => {setSearchTerm(e.target.value)})}                              /* Hakukenttään kirjoitetaan hakusana, joka lähtee backendiin */
-      name="search" placeholder="genre, name..." />                                         {/* useState muuttuu seSearchTermin perusteella inputin valueen katsoen */}
-      <button className="myForm" onClick={search}>Search</button>                                              
-      <div>
-        <ul>
-        {results.map(item => (
-          <li key={uuid()}>Title: {item.primary_title}<br /> Genre: {item.genre}</li>       /* Axioksella saatu taulukko mapataan listaan, jotta ne olisivat helpommin luettavissa */
-        ))}                                                                                 {/* vrt. JSON (hankala) vs HTML lista (selkeä) */}
-        </ul>
+      name="search" placeholder="Movie name or a part of it" />                                         {/* useState muuttuu seSearchTermin perusteella inputin valueen katsoen */}
+      <button className="myForm" onClick={search}>Search</button>  
+      <div className="col-12">
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Action" 
+          onChange={e => setRadioSearch(e.target.value)} />
+          <label className="form-check-label" >Action</label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Animation"
+          onChange={e => setRadioSearch(e.target.value)}  />
+          <label className="form-check-label" >Animation</label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Comedy"
+          onChange={e => setRadioSearch(e.target.value)}  />
+          <label className="form-check-label" >Comedy</label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Crime"
+          onChange={e => setRadioSearch(e.target.value)}  />
+          <label className="form-check-label" >Crime</label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Documentary"
+          onChange={e => setRadioSearch(e.target.value)}  />
+          <label className="form-check-label" >Documentary</label>
+        </div>
       </div>
+      <div>
+        <button className="funbutton" onClick={radio}>Search by genre</button>
+      <h2>Fun buttons for different uses</h2>
+        <button className="funbutton" onClick={fi_movies}>10 Finnish movies</button>      
+        <button className="funbutton" onClick={gb_movies}>10 English movies</button>  
+      </div>                         
+      <ul className="main">
+        <ul className="col-3">
+        {results.map(item => (
+          <li key={uuid()}>Title: {item.primary_title}<br /> {/* Axioksella saatu taulukko mapataan listaan, jotta ne olisivat helpommin luettavissa */}
+          Genre: {item.genre}<br />                               {/* vrt. JSON (hankala) vs HTML lista (selkeä) */}
+          Start year: {item.start_year}
+          </li>                            
+        ))}                             
+        </ul>
+        <ul className="col-3">
+          {viewResults.map(item => (
+            <li key={uuid()}>
+              Title: {item.title}<br />
+              Genre: {item.genre}<br />
+              Start year: {item.start_year}
+            </li>
+          ))}
+        </ul>
+      </ul>
     </div>
   )
 }
